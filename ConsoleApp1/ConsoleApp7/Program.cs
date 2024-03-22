54666270a1ce3420512530cb50a2cd066aec8981
김민정 <minjeoungk@naver.com>
2024-03-22 14:52:29 +0900

    작업파일 업데이트

---
+++ ConsoleApp1/ConsoleApp7/Program.cs
@@ -0,0 +1,38 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading;
+using System.Threading.Tasks;
+
+namespace ConsoleApp7
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            string start= "1";
+            for(int i = 0; i < 20; i++) {
+                Console.WriteLine(i+1+"번째수열:"+start);
+                char num =start[0];//읽은 숫자
+                string end = "";
+                int count = 0;
+                for(int j = 0; j < start.Length; j++)
+                {
+                    if (start[j] == num)
+                        count++;
+                    else
+                    {
+                        end=end + num + count;
+                        num = start[j];
+                        count = 1;
+                    }
+                }
+                end = end + num + count;
+                start = end;
+
+
+            }
+        }
+    }
+}
